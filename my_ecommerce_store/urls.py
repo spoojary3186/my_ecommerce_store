aac67e804194daf36d64cbd675ccf1b119fab836
spoojary3186 <spsujan2004@gmail.com>
2025-05-15 16:05:41 +0530

    Fixed urlpatterns order for static files

--- my_ecommerce_store/urls.py
+++ my_ecommerce_store/urls.py
@@ -1,8 +1,14 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.contrib import admin  # type: ignore
+from django.urls import path, include  # type: ignore
 from store.views import product_list  # Import the product list view
-urlpatterns = [
+from django.conf import settings  # type: ignore
+from django.conf.urls.static import static  # type: ignore
+
+urlpatterns = [  # Define main URL patterns first
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),  # Existing route for products
-    path('', product_list, name='home'),  #  This sets product_list as the homepage
+    path('', product_list, name='home'),  # This sets product_list as the homepage
 ]
+
+# Append static URL handling AFTER defining urlpatterns
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
